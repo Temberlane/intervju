4034ed124559285fd664b799cd9141a329403c74
Muhammad Sheikh <muhammad-sheikh@outlook.com>
2025-06-21 15:27:01 -0400

    added facial detection (racial coming next patch <3)

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,15 +1,18 @@
-import cv2
 import numpy as np
-from cv2 import VideoCapture, imshow, waitKey, destroyAllWindows
+from cv2 import VideoCapture, waitKey, destroyAllWindows
+from hsemotion_onnx.facial_emotions import HSEmotionRecognizer
 from ultralytics import YOLO
 
-# Create video capture object
+
+#VARIABLE DECLARATION
 capture = VideoCapture(0)
 ultralyticsModel = YOLO("yolo11m-pose.pt")
+emotionalModel = HSEmotionRecognizer(model_name="enet_b0_8_best_afew")
+net = cv2.dnn.readNetFromCaffe("deploy.prototxt",
+                               "res10_300x300_ssd_iter_140000_fp16.caffemodel")
 frame_skip = 1
 frame_id = 0
 maxDrop = 0.45
-
 BODY_NAMES = (
     "nose","l_eye","r_eye","l_ear","r_ear",
     "l_shoulder","r_shoulder","l_elbow","r_elbow",
@@ -23,18 +26,18 @@ SKELETON = [
 if not capture.isOpened():
     print("Error establishing connection")
 
+#MAIN LOOP
 while capture.isOpened():
-
     # Read an image frame
     validCapture, frame = capture.read()
-    #add frame skip
+    #------------------FRAME PROCESSING-----------------
     if frame_id % frame_skip == 0:
-        #use model to get results and add those to a flattened keypoint 2d map
+        #Use model to get results and add those to a flattened keypoint 2d map
         results = ultralyticsModel.predict(frame, conf=0.25, verbose=False)
         keyPoints = results[0].keypoints.xy
         vis = frame.copy()
 
-        #Slouch detection using midpoint ratio
+        #------------------SLOUCH DETECTION-----------------
         singlePersonKeyPoints = results[0].keypoints.xy[0]
         Lshoulder, Rshoulder, Nose = singlePersonKeyPoints[5], singlePersonKeyPoints[6], singlePersonKeyPoints[0]
         midX = (Lshoulder + Rshoulder) / 2
@@ -43,26 +46,54 @@ while capture.isOpened():
             #append json here
             cv2.putText(vis, "Slouch!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
+        # ------------------FACIAL DETECTION -----------------
+        #Make the frame for the face shape and make a blob for openCV
+        h, w = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                                     (104, 117, 123), False, False)
+        net.setInput(blob)
+        detections = net.forward()
 
-        #draws the skelton and person points
+        #Detect the face with the given bounds
+        for i in range(detections.shape[2]):
+            conf = float(detections[0, 0, i, 2])
+            if conf < 0.5:
+                continue
+            # Get bounding box in original frame coords
+            x1 = int(detections[0, 0, i, 3] * w)
+            y1 = int(detections[0, 0, i, 4] * h)
+            x2 = int(detections[0, 0, i, 5] * w)
+            y2 = int(detections[0, 0, i, 6] * h)
+            x1, y1 = max(0, x1), max(0, y1)
+            x2, y2 = min(w, x2), min(h, y2)
+
+            face = frame[y1:y2, x1:x2]
+            if face.size == 0:
+                continue
+            #Evaulte data with the HSemotions model and output into variables
+            label, scores = emotionalModel.predict_emotions(face[:, :, ::-1], logits=False)
+            order = ['anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise']
+            conf_emotion = float(scores[order.index(label.lower())])
+            #append json here
+            break
+
+        #------------------SKELETON RENDERING -----------------
         for person in results[0].keypoints.xy:
             for x, y in person:
                 cv2.circle(vis, (int(x), int(y)), 4, (255, 0, 0), -1)
-            for i, j in SKELETON:
-                x1, y1 = person[i]
-                x2, y2 = person[j]
-                cv2.line(vis, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        cv2.rectangle(vis, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(vis, f"{label}:{conf:.2f}", (x1, y2 + 25),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2)
+
 
 
         #draws frame
         cv2.imshow("Keypoints", vis)
-
+    # ------------------FRAME RENDERING-----------------
     frame_id = frame_id + 1
-    # If the Esc key is pressed, terminate the while loop
     if waitKey(25) == 27:
         break
 
 # Release the video capture and close the display window
 capture.release()
 destroyAllWindows()
-
