c89be92c6a375f606e6882087c7232a2fcbcb408
Thomas <60454907+Temberlane@users.noreply.github.com>
2025-06-22 01:43:02 -0400

    added webcam

--- frontend/intervju-frontend/src/pages/InterviewPage.js
+++ frontend/intervju-frontend/src/pages/InterviewPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./InterviewPage.css";
 import { useRive } from '@rive-app/react-canvas';
+import Webcam from "react-webcam";
 
 const STATE_MACHINE = "State Machine 1"; // Adjust if your state machine name is different
 
@@ -19,9 +20,22 @@ export default function InterviewPage() {
             <RiveComponent style={{ width: '100%', height: '100%' }} />
             
             <div className="interviewee">
-              {/* Placeholder for Interviewee */}
-              <p>User Recording</p>
-          </div>
+              {/* Webcam for User Recording */}
+              <Webcam
+                audio={false}
+                height="100%"
+                width="100%"
+                style={{
+                  borderRadius: '8px',
+                  objectFit: 'cover'
+                }}
+                videoConstraints={{
+                  width: 1280,
+                  height: 720,
+                  facingMode: "user"
+                }}
+              />
+            </div>
         </div>
       </div>
     </div>
